1fb80e4325055fae6a71c921dca62902e83e1fe1
me <pgard09@gmail.com>
2021-05-02 18:36:18 -0400

    working launch.json and threads

--- test-thread.c
+++ test-thread.c
@@ -7,11 +7,20 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-void thread_test()
+void *thread_test(void *arg)
 {
+    printf("hello from thread %lx \n", pthread_self());
 }
 
 int main(int argc, char *argv[])
 {
-    printf("sdg=ss");
+    pthread_t t1, t2, t3;
+    printf("main thread %lx starting a new thread \n", pthread_self());
+    pthread_create(&t1, NULL, thread_test, NULL);
+    pthread_create(&t2, NULL, thread_test, NULL);
+    pthread_create(&t3, NULL, thread_test, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+    return 0;
 }
